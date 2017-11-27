0b601ef10e3aa72e09bd42366a9f5a1acc26fec5
Peter Juhasz <jpeter82@gmail.com>
2017-11-27 05:08:38 +0100

    Add Author controller and view

---
+++ src/main/java/com/jpeter/firstspringapp/controller/AuthorController.java
@@ -0,0 +1,23 @@
+package com.jpeter.firstspringapp.controller;
+
+import com.jpeter.firstspringapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
